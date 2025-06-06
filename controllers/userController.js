e6b802956982aebe06388fb71d5c1c6a7f8dfc67
Nik-at-262992094974 <nikuyunfurqan@gmail.com>
2025-06-06 12:48:54 +0800

    convert sameSite to Lax

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,7 +28,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
